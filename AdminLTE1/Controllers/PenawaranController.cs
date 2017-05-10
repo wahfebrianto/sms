2740fef7a152b1d6e2ab39c7f2d930b6537c9e86
DESKTOP-HBR5RJM\MyLaptop <tancejang@gmail.com>
2017-05-10 09:40:24 +0700

    Added RFQ and Penawaran

---
+++ AdminLTE1/Controllers/PenawaranController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminLTE1.Controllers
+{
+    public class PenawaranController : Controller
+    {
+        // GET: Penawaran
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
