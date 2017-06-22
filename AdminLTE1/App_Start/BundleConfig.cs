c4dfe696075c3c3b75aab53d28b9e45d4f45e1a6
Wahyu <wahfebrianto@gmail.com>
2017-06-22 23:38:01 +0700

    FindPrice

--- AdminLTE1/App_Start/BundleConfig.cs
+++ AdminLTE1/App_Start/BundleConfig.cs
@@ -9,36 +9,39 @@ namespace AdminLTE1.App_Start
             bundles.Add(new StyleBundle("~/Bundles/css")
                 .Include("~/Content/css/bootstrap.css")
                 .Include("~/Content/css/select2.css")
-                .Include("~/Content/css/datepicker3.css")
                 .Include("~/Content/css/AdminLTE.css")
                 .Include("~/Content/css/skins/skin-blue.css")
                 .Include("~/Content/bootstrap.min.css")
                 .Include("~/Scripts/plugins/jquery-ui/jquery-ui.min.css")
                 .Include("~/Content/animate.css")
                 .Include("~/Content/style.css")
+                .Include("~/Content/css/bootstrap-select.css")
                 .Include("~/fonts/font-awesome/css/font-awesome.min.css")
                 .Include("~/Content/plugins/dataTables/datatables.min.css")
-                .Include("~/Content/plugins/sweetalert/sweetalert.css"));
+                .Include("~/Content/plugins/sweetalert/sweetalert.css")
+                .Include("~/Content/css/datepicker3.css")
+                .Include("~/Content/css/bootstrap-datetimepicker.css"));
 
             bundles.Add(new ScriptBundle("~/Bundles/js")
                 .Include("~/Content/js/plugins/jquery/jquery-2.2.4.js")
-                .Include("~/Content/js/plugins/bootstrap/bootstrap.js")
+                //.Include("~/Content/js/plugins/bootstrap/bootstrap.js")
                 .Include("~/Content/js/plugins/fastclick/fastclick.js")
                 .Include("~/Content/js/plugins/slimscroll/jquery.slimscroll.js")
                 .Include("~/Content/js/plugins/select2/select2.full.js")
                 .Include("~/Content/js/plugins/moment/moment.js")
-                .Include("~/Content/js/plugins/datepicker/bootstrap-datepicker.js")
                 .Include("~/Content/js/plugins/icheck/icheck.js")
                 .Include("~/Content/js/plugins/validator.js")
                 .Include("~/Content/js/plugins/inputmask/jquery.inputmask.bundle.js")
                 .Include("~/Content/js/app.js")
                 .Include("~/Content/js/init.js")
-                //.Include("~/Scripts/jquery-3.1.1.min.js")
+                .Include("~/Content/js/plugins/jqueryNumber/jquery.number.min.js")
                 .Include("~/Scripts/plugins/jquery-ui/jquery-ui.min.js")
                 .Include("~/Scripts/bootstrap.min.js")
+                .Include("~/Scripts/bootstrap-select.js")
                 .Include("~/Scripts/plugins/dataTables/datatables.min.js")
                 .Include("~/Scripts/plugins/metisMenu/metisMenu.min.js","~/Scripts/plugins/pace/pace.min.js","~/Scripts/app/inspinia.js")
-                .Include("~/Scripts/plugins/sweetalert/sweetalert.min.js"));
+                .Include("~/Scripts/plugins/sweetalert/sweetalert.min.js")
+                .Include("~/Content/js/plugins/datepicker/bootstrap-datetimepicker.min.js"));
 
 #if DEBUG
             BundleTable.EnableOptimizations = false;
